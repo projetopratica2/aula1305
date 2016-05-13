c3739b2937b3ea1f95a39094d04befff0ad6105d
Francisco <francisco.junior@jaboatao.ifpe.edu.br>
2016-05-13 10:13:42 -0300

    Adding entity Usuario

--- src/basicas/Usuario.java
+++ src/basicas/Usuario.java
@@ -2,5 +2,52 @@ package basicas;
 
 public class Usuario {
 
+	private Integer codigo;
+	private String login;
+	private String senha;
+	private String nome;
+	private String email;
+	
+	public Usuario(){
+		
+	}
+	public Usuario(Integer codigo, String login, String senha, String nome, String email) {
+		super();
+		this.codigo = codigo;
+		this.login = login;
+		this.senha = senha;
+		this.nome = nome;
+		this.email = email;
+	}
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
